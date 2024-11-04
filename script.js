bb7279eaa0645b4b743d542b81bc10dadb44cca1
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2024-11-04 09:36:16 +0100

    Create features.md
    Added index.html for testing squashing git commits
    
    Added index.html for testing squashing git commits
    
    JavaScript functionality and fix issues
    
    a

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+
+console.log("This is a test without --fores-with-lease");
+console.log("This is a test --fores-with-lease");
+
+console.log("try to commit sqush to a different branch");
